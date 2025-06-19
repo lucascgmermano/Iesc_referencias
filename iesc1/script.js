5c66959d7becc9c77f6fb0cea8932475e3e447d6
Lucas Coraça Germano <lucascgermano@gmail.com>
2025-06-19 18:40:49 -0300

    Corrige CSS responsivo

--- iesc1/script.js
+++ iesc1/script.js
@@ -1,4 +1,16 @@
-function toggleMenu() {
-    const sidebar = document.querySelector('.sidebar');
-    sidebar.classList.toggle('hidden');
+function loadContent(page) {
+    fetch(page)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Página não encontrada');
+            }
+            return response.text();
+        })
+        .then(data => {
+            document.getElementById('content').innerHTML = data;
+        })
+        .catch(error => {
+            document.getElementById('content').innerHTML = "<p>Erro ao carregar a página.</p>";
+            console.error(error);
+        });
 }
